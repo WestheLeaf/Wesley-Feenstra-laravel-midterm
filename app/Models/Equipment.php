19835314013471d1f5748990b47ccaf395c6a4ae
WestheLeaf <92562761+WestheLeaf@users.noreply.github.com>
2022-03-29 12:16:59 -0700

    g

---
+++ app/Models/Equipment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class equipment extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'price', 'ram', 'gpu', 'category', 'manufacturer_id'];
+
+    protected $table = 'equipments';
+
+    public function manufacturer()
+    {
+        return $this->belongsTo(Manufacturer::class);
+    }
+
+    public function invoices()
+    {
+        return $this->belongsToMany(Invoice::class, 'invoice_equipment');
+    }
+}
---
+++ app/Models/Manufacturer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class manufacturer extends Model
+{
+    use HasFactory;
+    protected $fillable = ['sales_phone', 'tech_phone'];
+
+    public function equipments()
+    {
+        return $this->hasMany(Equipment::class);
+    }
+}
