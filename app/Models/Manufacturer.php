eaab4a17478f6e7b2c0501306df28c1515d222f6
WestheLeaf <92562761+WestheLeaf@users.noreply.github.com>
2022-03-29 20:29:13 -0700

    g

--- app/Models/Manufacturer.php
+++ app/Models/Manufacturer.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class manufacturer extends Model
+class Manufacturer extends Model
 {
     use HasFactory;
     protected $fillable = ['sales_phone', 'tech_phone'];
