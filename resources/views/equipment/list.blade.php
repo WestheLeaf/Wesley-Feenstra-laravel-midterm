ec21b0439165e9280caa38b0e619dc8519e6ea78
WestheLeaf <92562761+WestheLeaf@users.noreply.github.com>
2022-03-29 21:22:09 -0700

    g

---
+++ resources/views/equipment/list.blade.php
@@ -0,0 +1,31 @@
+@extends('adminlte::page')
+
+@section('title', 'Customers')
+
+@section('content_header')
+    <h1>Customers</h1>
+@stop
+
+@section('content')
+<div class="card">
+  <div class="card-body">
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th style="width: 10px">#</th><th>Task</th><th style="width: 40px">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($equipments AS $equipment)
+        <tr>
+          <td>{{ $equipment->id }}</td>
+          <td>{{ $equipment->name }}</td>
+          <td><a class="btn btn-default btn-sm" href="{{ route('equipment.show',['equipment'=>$equipment->id]) }}">View</a></td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
+<a href="{{ route('equipment.create') }} " class="btn btn-primary" >Create</a>
+@stop
